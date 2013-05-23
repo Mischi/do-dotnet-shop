2443da3d7fd749a6342da20cc29f836c95f99b0d
Fabian Raetz <fabian.raetz@gmail.com>
2013-05-23 21:11:53 +0200

    angular module

---
+++ DoDotNetShop.Web/App/ShopBackend/controllers.js
@@ -0,0 +1,17 @@
+﻿var shopBackend = angular.module('donetshop.backend', []);
+
+shopBackend.controller('EditProductCtrl', ['$scope',
+    
+    function EditProductCtrl($scope) {
+        'use strict';
+
+        $scope.categories = ['Software', 'Hardware', 'Food', 'Drinks'];
+        $scope.products = [];
+
+        $scope.saveProduct = function (product) {
+            $scope.products.push(product);
+            $scope.product = null;
+        };
+    }
+
+]);
